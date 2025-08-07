9b4aeeb79a281ec3e8ce5cd6029441f01cfaf80f
Pandox Mando <pandoxmando@gmail.com>
2025-08-07 10:25:41 +0300

    worked on models

---
+++ asset/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+CATEGORIES = [("LPT", "Laptop"), ("MOB", "Mobile Phone"), ("PRT", "Printer"), ("DSK", "Desktop"), ("AIO", "All In One"), ("TV", "Television")]
+# Create your models here.
+class Asset(models.Model):
+    code = models.CharField(max_length=100, null=False)
+    serial_number = models.CharField(max_length=100, null=False)
+    specs = models.TextField(max_length=100, null=False)
+    category = models.CharField(max_length=50, null=False, choices=CATEGORIES)
+
+    def __init__(self):
+        return f'{self.serial_number} - {self.category}'
--- intranet/settings.py
+++ intranet/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     # Local
     "accounts",
     "pages",
+    'staff',
 ]
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#middleware
@@ -56,10 +57,10 @@ MIDDLEWARE = [
 ]
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#root-urlconf
-ROOT_URLCONF = "django_project.urls"
+ROOT_URLCONF = "intranet.urls"
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
-WSGI_APPLICATION = "django_project.wsgi.application"
+WSGI_APPLICATION = "intranet.wsgi.application"
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#templates
 TEMPLATES = [
