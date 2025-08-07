ad7f4a2fc2c93ac940215df4efd96bda08c3e060
Intern IT <internit@3ways_kla.local>
2025-08-07 14:19:51 +0300

    commit 1

--- asset/views.py
+++ asset/views.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 class assetHomePageView(TemplateView):
     template_name = "asset/home.html"
 
-    class assetDetailsView(TemplateView):
-        template_name = "asset/details.html"
+class assetDetailsView(TemplateView):
+    template_name = "asset/details.html"
--- intranet/urls.py
+++ intranet/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include("pages.urls")),
+    path("asset/", include("asset.urls")),
+    path("handover/", include("handover.urls")),
+    path("staff/", include("staff.urls")),
+
 ]
 
 if settings.DEBUG:
