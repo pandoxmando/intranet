94c889970bf17eec675bd12111316d3cefb02bf2
Pandox Mando <pandoxmando@gmail.com>
2025-08-07 14:48:25 +0300

    worked on forms2

--- asset/views.py
+++ asset/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from .models import Asset
 
 
 class assetHomePageView(TemplateView):
@@ -6,3 +8,11 @@ class assetHomePageView(TemplateView):
 
 class assetDetailsView(TemplateView):
     template_name = "asset/details.html"
+
+class AssetList(request):
+    assets = Asset.objects.all()
+    # return render(request, 'assets/asset_list.html', {'assets': assets})
+
+# class AssetDetailView(request, serial_number):
+#     asset = Asset.objects.get(serial_number=serial_number)
+#     return render(request, 'assets/asset_details.html', {'asset': asset})
